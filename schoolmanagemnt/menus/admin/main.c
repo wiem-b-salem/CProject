5cf8874cb693aecf089cbff6444219c2ff3437f1
wiem-b-salem <bensalemwi2m@gmail.com>
2024-12-14 18:06:57 +0100

    Add files via upload

--- schoolmanagemnt/menus/admin/main.c
+++ schoolmanagemnt/menus/admin/main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    char tabchoix[10][10]={"Enseignant","Parent","Etudiant","Classe","Cours","Emploi","Evenement","Club","Notes","Profile"};
+    char tabchoix[10][15]={"Enseignant","Parent","Etudiant","Classe","Cours","Emploi","Evenement","Club","Notes","Profile"};
     int choix;
     do
     {
@@ -10,11 +10,11 @@ int main()
         printf("Selectionnez votre choix \n");
         for (int i = 0; i < 10; i++)
         {
-            printf("%d - Gerer %s\n",i,tabchoix[i]);
+            printf("%d - Gerer %s \n",i+1,tabchoix[i]);
         }
         printf("0 - QUITTER");
         scanf("%d",&choix);
-    } while (choix<0 || choix>2);
+    } while (choix<0 || choix>10);
     
 
 }
